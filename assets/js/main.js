93e1f81694cb95e29bdb7429923c3927f625e08e
programinf <programinf@gmail.com>
2017-03-25 00:07:55 +0300

    edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -392,3 +392,14 @@ $(function(){
     });
 });
 //Slider end
+
+$(document).ready(function(){
+
+
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+
+});
