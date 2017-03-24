db82e88bc9cfad18aa9de388b0d14b5accfdcaa4
programinf <programinf@gmail.com>
2017-03-24 22:59:12 +0300

    edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 
 	skel.breakpoints({
 		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
+		large: '(max-width: 1270px)',
 		medium: '(max-width: 980px)',
 		small: '(max-width: 736px)',
 		xsmall: '(max-width: 480px)',
